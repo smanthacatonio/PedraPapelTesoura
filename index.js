0741da01a68c61e169e4534c9dd277d4b597a9d8
samanthacatonio <samanthacatonio@hotmail.com>
2023-04-09 18:51:15 -0400

    html disputa

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+const pickUserHand = (hand) => {
+    console.log(hand);
+    //esconder a página atual
+    let hands = document.querySelector(".hands");
+    hands.style.display = "none";
+    //mostrar a próxima página com a mão que eu escolhi
+    
+
+}
